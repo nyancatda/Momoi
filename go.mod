539cd0f5bc87d6d98d76bf3451220718ed165990
NyanCatda <nyancatda@outlook.com>
2022-09-09 22:17:22 +0800

    完成获取Socket5代理列表

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/nyancatda/Momoi
+
+go 1.18
+
+require (
+	github.com/nyancatda/AyaLog v1.0.8
+	github.com/nyancatda/HttpRequest v1.0.4
+)
---
+++ main.go
@@ -0,0 +1,13 @@
+/*
+ * @Author: NyanCatda
+ * @Date: 2022-09-09 21:56:24
+ * @LastEditTime: 2022-09-09 22:16:40
+ * @LastEditors: NyanCatda
+ * @Description: 主文件
+ * @FilePath: \Momoi\main.go
+ */
+package main
+
+func main() {
+
+}
