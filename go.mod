35476c8403c0210409a4b67253e8c4cde11afee8
NyanCatda <nyancatda@outlook.com>
2023-12-13 14:06:35 +0800

    Init Project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nyancatda/Momoi/v2
+
+go 1.21.5
---
+++ main.go
@@ -0,0 +1,9 @@
+/*
+ * @Author: NyanCatda
+ * @Date: 2023-12-13 14:05:12
+ * @LastEditTime: 2023-12-13 14:05:16
+ * @LastEditors: NyanCatda
+ * @Description: main.go
+ * @FilePath: \Momoi\main.go
+ */
+package main
